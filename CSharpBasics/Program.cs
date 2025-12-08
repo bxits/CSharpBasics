006949145420db61350f717381e774d9da367db9
bxits <ruediger.berg@its-stuttgart.de>
2025-12-08 08:30:15 +0100

    Initial Übungs- und Demoaufgaben in C#.

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CSharpBasics
+{
+    internal class Program
+    {
+        static string[] names = new string[]
+{
+    "Aiden", "Olivia", "Sofia", "Jackson", "Liam",
+    "Emma", "Isabella", "Mason", "Lucas", "Ava",
+    "Ethan", "Mia", "Harper", "Logan", "Amelia",
+    "James", "Evelyn", "Benjamin", "Ella", "Alexander",
+    "Charlotte", "Michael", "Elijah", "Abigail", "Daniel",
+    "Emily", "Matthew", "Chloe", "Samuel", "Scarlett"
+};
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ///Vertauschen Sie das 14. und 15. Element.
+            ///
+
+            ///Geben Sie die Namen zeilenweise auf der Console aus.
+            ///Schreiben sie hierfür eine eigene Methode.
+
+            ///Sortieren Sie die Namen manuell mittels einer for-Schleife.
+            ///
+
+            ///Sortieren Sie die Namen rückwärts mittels einer foreach-Schleife.
+            ///
+
+            ///Erzeugen Sie eine zufällige Mischen mittels Random-Generator.
+            ///
+
+            ///Higher-Oder-Functions
+            ///1a. Sortieren Sie die Liste nache Namen.
+            ///1b. ...nach Länge der Namen
+            ///2. Untersuchen Sie, ob der Name "Lucy" in den Namen vorkommt.
+            ///
+
+            ///Wandeln Sie das Array in eine Liste um.
+            ///Wandeln Sie das Array in ein Dictionary um.
+            ///--> Nennen Sie den jeweiligen Vorteil/Nachteil und stellen Sie die Struktur der Daten dar.
+
+
+        }
+    }
+}
